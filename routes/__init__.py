365b2ccd73fd3ee31880606357bc3d2a0e713145
Heraklos Visha <heraklosvisha13@gmail.com>
2026-08-25 09:49:24 +0700

    fix(routes): correct import path for auth_bp and users_bp

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,5 @@
 # Import semua routes/blueprint di sini agar bisa diakses dari luar
-from .auth_routes import auth_bp, users_bp
+from .user_routes import auth_bp, users_bp
 from .product_routes import products_bp
 from .category_routes import categories_bp
 from .order_routes import orders_bp
